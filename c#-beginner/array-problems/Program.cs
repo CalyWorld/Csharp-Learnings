e3053e3a0563ccc5bb41882177e875211f3814a3
callistus <obinnaanizoba5@gmail.com>
2024-09-26 14:09:20 +0800

    update

---
+++ c#-beginner/array-problems/Program.cs
@@ -0,0 +1,59 @@
+﻿// /*
+// string[] fraudulentOrderIDs = new string[3];
+
+// fraudulentOrderIDs[0] = "A123";
+// fraudulentOrderIDs[1] = "B456";
+// fraudulentOrderIDs[2] = "C789";
+// // fraudulentOrderIDs[3] = "D000";
+// */
+
+// // string[] fraudulentOrderIDs = [ "A123", "B456", "C789" ];
+
+// // Console.WriteLine($"First: {fraudulentOrderIDs[0]}");
+// // Console.WriteLine($"Second: {fraudulentOrderIDs[1]}");
+// // Console.WriteLine($"Third: {fraudulentOrderIDs[2]}");
+
+// // fraudulentOrderIDs[0] = "F000";
+
+// // Console.WriteLine($"Reassign First: {fraudulentOrderIDs[0]}");
+
+// // Console.WriteLine($"There are {fraudulentOrderIDs.Length} fraudulent orders to process.");
+
+
+// // string[] names = {"Rowena", "Robin", "Bao"};
+// // foreach(string name in names){
+// //     Console.WriteLine(name);
+// // }
+
+// int[] inventory = {200, 450, 700, 175, 250};
+// int sum = 0;
+// int bin = 0;
+// foreach(int items in inventory){
+//     sum+=items;
+//     bin++;
+//     Console.WriteLine($"Bin {bin} = {items} items (Running total: {sum})");
+// }
+// Console.WriteLine($"We have {sum} items in our inventory");
+
+//code challenge
+
+// string[] orderIDs = {"B123", "C234", "A345", "C15", "B177", "G3003", "C235", "B179"};
+
+// foreach(string orderID in orderIDs){
+//     if(orderID.StartsWith("B")){
+//         Console.WriteLine($"The name starts with 'B' ! {orderID}");
+//     }
+// }
+
+// Random dice = new Random();
+// int roll = dice.Next(1, 7);
+// Console.WriteLine(roll);
+
+Random dice = new Random();
+int roll1 = dice.Next();
+int roll2 = dice.Next(101);
+int roll3 = dice.Next(50, 101);
+
+Console.WriteLine($"First roll: {roll1}");
+Console.WriteLine($"Second roll: {roll2}");
+Console.WriteLine($"Third roll: {roll3}");
---
+++ c#-beginner/if-else-challenge/Program.cs
@@ -0,0 +1,88 @@
+﻿// // /*Random random = new Random();
+// // int daysUntilExpiration = random.Next(12);
+// // int discountPercentage = 0;
+
+// // if (daysUntilExpiration == 0)
+// // {
+// //     Console.WriteLine("Your subscription has expired.");
+// // }
+// // else if (daysUntilExpiration == 1)
+// // {
+// //     Console.WriteLine("Your subscription expires within a day!");
+// //     discountPercentage = 20;
+// // }
+// // else if (daysUntilExpiration <= 5)
+// // {
+// //     Console.WriteLine($"Your subscription expires in {daysUntilExpiration} days.");
+// //     discountPercentage = 10;
+// // }
+// // else if (daysUntilExpiration <= 10)
+// // {
+// //     Console.WriteLine("Your subscription will expire soon. Renew now!");
+// // }
+
+// // if (discountPercentage > 0)
+// // {
+// //     Console.WriteLine($"Renew now and save {discountPercentage}%.");
+// // } */
+
+// // Random random = new Random();
+// // string[] orderIDs = new string[5];
+// // // Loop through each blank orderID
+// // for (int i = 0; i < orderIDs.Length; i++)
+// // {
+// //     // Get a random value that equates to ASCII letters A through E
+// //     int prefixValue = random.Next(65, 70);
+// //     // Convert the random value into a char, then a string
+// //     string prefix = Convert.ToChar(prefixValue).ToString();
+// //     // Create a random number, pad with zeroes
+// //     string suffix = random.Next(1, 1000).ToString("000");
+// //     Console.WriteLine("" + suffix);
+// //     // Combine the prefix and suffix together, then assign to current OrderID
+// //     orderIDs[i] = prefix + suffix;
+// // }
+// // // Print out each orderID
+// // foreach (var orderID in orderIDs)
+// // {
+// //     Console.WriteLine(orderID);
+// // }
+
+// string str = "The quick brown fox jumps over the lazy dog.";
+// // convert the message into a char array
+// char[] charMessage = str.ToCharArray();
+// // Reverse the chars
+// Array.Reverse(charMessage);
+// int x = 0;
+// // count the o's
+// foreach (char i in charMessage) { if (i == 'o') { x++; } }
+// // convert it back to a string
+// string new_message = new String(charMessage);
+// // print it out
+// Console.WriteLine(new_message);
+// Console.WriteLine($"'o' appears {x} times.");
+
+/*
+   This code reverses a message, counts the number of times 
+   a particular character appears, then prints the results
+   to the console window.
+ */
+
+string originalMessage = "The quick brown fox jumps over the lazy dog.";
+
+char[] message = originalMessage.ToCharArray();
+Array.Reverse(message);
+
+int letterCount = 0;
+
+foreach (char letter in message)
+{
+    if (letter == 'o')
+    {
+        letterCount++;
+    }
+}
+
+string newMessage = new String(message);
+
+Console.WriteLine(newMessage);
+Console.WriteLine($"'o' appears {letterCount} times.");
