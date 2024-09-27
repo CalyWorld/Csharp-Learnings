9b0d483c430bb9d76f5d6682fa9bc340fe4ce637
callistus <obinnaanizoba5@gmail.com>
2024-09-27 19:58:41 +0800

    update

---
+++ c#-beginner/coin-challenge-conditional-operator/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+// Random rand = new Random();
+// int dice = rand.Next(0, 2);
+
+/* using conditional operator*/
+
+// Console.WriteLine((dice == 0) ? "heads" : "tails");
+
+/* Complete a challenge activity using Boolean expressions */
+
+string permission = "Admin | Manager";
+int level = 55;
+
+Console.WriteLine(
+    (permission.Contains("Admin") && level > 55) ? "Welcome, Super Admin user." : 
+    (permission.Contains("Admin") && level <= 55 )?  "Welcome, Admin user." : 
+    (permission.Contains("Manager") && level >= 20 )?  "Contact an Admin for access." : 
+    (permission.Contains("Manager") && level < 20 )?  "You do not have sufficient privileges." : 
+    "You do not have sufficient privileges.");
+
---
+++ c#-beginner/evaluating equality and inequality/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("a" == "a"); //True
+// Console.WriteLine("a" == "A"); //False
+// Console.WriteLine(1 == 2); // False
+
+// string myValue = "a";
+// Console.WriteLine(myValue == "a"); //True
+// Console.WriteLine("a" == "a "); //False
+// string value1 = " a";
+// string value2 = "A ";
+// Console.WriteLine(value1.Trim().ToLower() == value2.Trim().ToLower()); //True
+
+
+// Console.WriteLine("a" != "a"); //false
+// Console.WriteLine("a" != "A"); //true
+// Console.WriteLine(1 != 2); //true
+
+// string myValue = "a";
+// Console.WriteLine(myValue != "a"); //false
+
+// Console.WriteLine(1 > 2);  //false
+// Console.WriteLine(1 < 2);  //true
+// Console.WriteLine(1 >= 1); //true
+// Console.WriteLine(1 <= 1); //true
