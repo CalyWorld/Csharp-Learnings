66307115be79fe2adfe50c351dfd2627f7e5c975
callistus <obinnaanizoba5@gmail.com>
2024-09-30 17:16:48 +0800

    fizz-buzz challenge

---
+++ c#-beginner/fizzBuzz-challenge/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+for(int i = 1; i < 101; i++){
+    // Console.WriteLine("" + i);
+    if((i % 3 == 0) && (i % 5 == 0)) Console.WriteLine($"{i} - fizzBuzz");
+    else if(i % 3 == 0) Console.WriteLine($"{i} - fizz");
+    else if(i % 5 == 0) Console.Write($"{i} - Buzz\n");
+    else Console.WriteLine($"{i}");
+}
